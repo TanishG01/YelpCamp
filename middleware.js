bfd4b9d1155d6924f12309d2a979273d41bb98aa
TanishG01 <tanishg543@gmail.com>
2026-09-08 16:23:17 +0530

    Controllers & star rating

--- middleware.js
+++ middleware.js
@@ -36,7 +36,7 @@ module.exports.validateCampground = (req,res,next) => {
 module.exports.isAuthor = async(req,res,next) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
-    if(!campground.author.equals(eeq.user._id)){
+    if(!campground.author.equals(req.user._id)){
         req.flash('error','You do not have permisssions to do that!!');
         return res.redirect(`/campgrounds/${id}`);
     }
@@ -46,7 +46,7 @@ module.exports.isAuthor = async(req,res,next) => {
 module.exports.isReviewAuthor = async(req,res,next) => {
     const {id,reviewId} = req.params;
     const review = await Review.findById(reviewId);
-    if(!review.author.equals(eeq.user._id)){
+    if(!review.author.equals(req.user._id)){
         req.flash('error','You do not have permisssions to do that!!');
         return res.redirect(`/campgrounds/${id}`);
     }
