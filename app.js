9a8b2b65a2a305681d049bbbea693e07e8c0cbaf
TanishG01 <tanishg543@gmail.com>
2026-09-11 19:18:37 +0530

    db and deploy time

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const { MongoStore } = require('connect-mongo');
+
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"connection error:"));
@@ -41,9 +44,24 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(sanitizeV5({ replaceWith: '_' }));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
+
+store.on("error", function(e){
+    console.log("SESSION STORE ERROR", e);
+})
+
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -133,6 +151,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err});
 })
 
-app.listen(3000, () =>{
-    console.log('Serving on Port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () =>{
+    console.log(`Serving on Port ${port}`);
 })
--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,8 @@ const cities = require('./cities');
 const {places,descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"connection error:"));
