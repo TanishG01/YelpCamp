8b90a83020596a9011d184d10c20e7bd07670e6d
TanishG01 <tanishg543@gmail.com>
2026-09-11 19:51:05 +0530

    seeding data in deploy version

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
-            author: '6a9d4e1753af3244a850f0e8',
+            author: '6aa40cb8cd4444e2b57552ad',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolore ipsa autem nobis quidem id vitae! Iusto, tempore quod sequi quo quibusdam praesentium consectetur non similique doloribus maiores atque sed!',
